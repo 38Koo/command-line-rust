c82d36fa258c3e8ca965e467e465012519d5b93e
宮本航平 <captain.marvel.mk@gmail.com>
2024-08-31 22:58:38 +0900

    echor の実装

---
+++ tests/echor_test.rs
@@ -0,0 +1,42 @@
+// cargo test --test echor_test
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+
+#[test]
+fn failed_no_args() -> TestResult {
+  assert_cmd::Command::cargo_bin("echor")?
+    .assert()
+    .failure()
+    .stderr(predicates::prelude::predicate::str::contains("USAGE"));
+  Ok(())
+}
+
+fn runs(args: &[&str], expected_file: &str) -> TestResult{
+  let expected = std::fs::read_to_string(expected_file)?;
+  assert_cmd::Command::cargo_bin("echor")?
+    .args(args)
+    .assert()
+    .success()
+    .stdout(expected);
+  Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+  runs(&["Hello there"], "tests/echor/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+  runs(&["Hello", "there"], "tests/echor/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+  runs(&["Hello there", "-n"], "tests/echor/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+  runs(&["-n", "Hello", "there"], "tests/echor/expected/hello2.n.txt")
+}
