835b18efe2c89989f555e990d34985bfd49d0d5b
宮本航平 <captain.marvel.mk@gmail.com>
2024-09-08 15:40:15 +0900

    複数コマンドラインを管理するためにディレクトリ構造の変更

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -3,8 +3,7 @@ use std::error::Error;
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
-    Ok(())
+  println!("Hello, world!");
+  Ok(())
 }
 
-pub mod catr; // catr.rs を読み込み
--- src/bin/catr/catr.rs
+++
@@ -1,5 +0,0 @@
-// ここに catr モジュールの実装を書く
-pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Running catr!");
-    Ok(())
-}
